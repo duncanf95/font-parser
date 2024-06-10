7084891b9263f923acefbbf70ca3e6d529e7e8b1
Duncan Fraser <duncan.fraser@pelion.com>
2024-06-10 16:38:46 +0100

    converts table tags to bytes, fixes issue not pulling in correct data for records

--- src/lib.rs
+++ src/lib.rs
@@ -6,8 +6,7 @@ mod parser;
 pub fn parse(font_file: &str) {
     let mut bytes = read_file(font_file);
     let table_directory = TableDirectory::new(&mut bytes);
-
-    println!("test");
+    table_directory.match_tables();
 }
 
 
--- src/parser/tables.rs
+++ src/parser/tables.rs
@@ -11,6 +11,7 @@ impl TableDirectory {
     pub fn new(font_data: &mut Vec<u8>) -> Self {
         let (sfnt_version, mut font_data) = ByteConverter::get_next_u32(font_data);
         let (num_tables, mut font_data) = ByteConverter::get_next_u16(&mut font_data);
+        font_data = font_data.split_off(6);
         let mut table_records: Vec<TableRecord> = Vec::new();
 
         for i in 0..num_tables {
@@ -25,6 +26,56 @@ impl TableDirectory {
             table_records
         }
     }
+
+    pub fn match_tables(&self) {
+        for table_record in &self.table_records {
+            match &table_record.tag {
+                b"bdat" => println!("table is bdat"),
+                b"bloc" => println!("table is bloc"),
+                b"CFF " => println!("table is CFF"),
+                b"CBDT" => println!("table is CBDT"),
+                b"CBLC" => println!("table is CBLC"),
+                b"CFF2" => println!("table is CFF2"),
+                b"COLR" => println!("table is COLR"),
+                b"CPAL" => println!("table is CPAL"),
+                b"EBDT" => println!("table is EBDT"),
+                b"EBLC" => println!("table is EBLC"),
+                b"GDEF" => println!("table is GDEF"),
+                b"GPOS" => println!("table is GPOS"),
+                b"GSUB" => println!("table is GSUB"),
+                b"MATH" => println!("table is MATH"),
+                b"HVAR" => println!("table is HVAR"),
+                b"MVAR" => println!("table is MVAR"),
+                b"OS/2" => println!("table is OS/2"),
+                b"SVG " => println!("table is SVG "),
+                b"VORG" => println!("table is VORG"),
+                b"VVAR" => println!("table is VVAR"),
+                b"ankr" => println!("table is ankr"),
+                b"avar" => println!("table is avar"),
+                b"cmap" => println!("table is cmap"),
+                b"feat" => println!("table is feat"),
+                b"fvar" => println!("table is fvar"),
+                b"glyf" => println!("table is glyf"),
+                b"gvar" => println!("table is gvar"),
+                b"head" => println!("table is head"),
+                b"hhea" => println!("table is hhea"),
+                b"hmtx" => println!("table is hmtx"),
+                b"kern" => println!("table is kern"),
+                b"kerx" => println!("table is kerx"),
+                b"loca" => println!("table is loca"),
+                b"maxp" => println!("table is maxp"),
+                b"morx" => println!("table is morx"),
+                b"name" => println!("table is name"),
+                b"post" => println!("table is post"),
+                b"sbix" => println!("table is sbix"),
+                b"trak" => println!("table is trak"),
+                b"vhea" => println!("table is vhea"),
+                b"vmtx" => println!("table is vmtx"),
+                _=> println!("Unknown Table!")
+            }
+        }
+    }
+
 }
 
 pub struct TableRecord {
